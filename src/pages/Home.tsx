33659e58320ba60bff6f99ddf1baf75b19c97e5d
jgpark-1 <jgpark7281@gmail.com>
2025-03-04 00:58:12 +0900

    chore: project vite react-ts init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="w-full max-w-md mx-auto p-6 flex items-center justify-center flex-col h-screen">
+      <h1 className="text-2xl font-bold text-rose-700">
+        Vite React TS Tailwind Template
+      </h1>
+    </div>
+  )
+}
+
+export default Home
